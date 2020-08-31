a72f63cffdc894de502a4a60c0c39b13c1ffa841
milton-sesarego <milton.sesarego@gmail.com>
2020-08-31 13:20:26 -0300

    pagina principal

---
+++ public/javascripts/map.js
@@ -0,0 +1,5 @@
+var map = L.map('main_map').setView([-34.67359531466894,-58.645599655906736], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
